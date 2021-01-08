898b4ff8073d337ee58864f5c808b03b0c364a10
Rachit Pratap Singh <62217874+kingrps@users.noreply.github.com>
2021-01-08 15:24:35 +0530

    Create FastScanner.java

---
+++ FastScanner.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class {
+
+	public static void main(String[] args) {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = in.nextInt();
+        while(t-->0) { 
+        	
+        }
+        out.flush();
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while(!st.hasMoreTokens())
+				try { st = new StringTokenizer(br.readLine()); }
+				catch(IOException e) {}
+			return st.nextToken();
+		}
+		
+		String nextLine(){
+			try{ return br.readLine(); } 
+			catch(IOException e) { } return "";
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		int[] readArray(int n) {
+			int a[] = new int[n];
+			for(int i=0;i<n;i++) a[i] = nextInt();
+			return a;
+		}
+	}
+
+	static final Random random = new Random();
+
+	static void ruffleSort(int[] a){
+		int n = a.length;
+		for(int i=0;i<n;i++){
+			int j = random.nextInt(n), temp = a[j];
+			a[j] = a[i]; a[i] = temp;
+		}
+		Arrays.sort(a); 	
+	}
+}
